9ec63043f89fabf78e42d2682333ac0d4849d773
Konstantin Grachev <kgrachev@gmail.com>
2025-03-23 21:54:12 +0000

    fix label styling

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -15,7 +15,7 @@ export interface SelectProps<T> {
   disabled?: boolean;
 }
 
-export function Select<T>({
+export function Select<T> ({
   value,
   onChange,
   children,
@@ -25,35 +25,37 @@ export function Select<T>({
   disabled = false,
 }: SelectProps<T>) {
   return (
-    <Field className={twMerge("relative w-full", className)}>
-      {label && (
-        <Label className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-200 mb-1">
-          {label}
-        </Label>
-      )}
-      <Listbox value={value} onChange={onChange} disabled={disabled}>
-        <div className="relative">
-          <ListboxButton className="relative w-full cursor-default rounded-md bg-white dark:bg-gray-800 py-2 pl-3 pr-10 text-left text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6">
-            {selectedOption}
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </span>
-          </ListboxButton>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-gray-800 py-1 text-base shadow-lg ring-1 ring-black dark:ring-gray-700 ring-opacity-5 focus:outline-none sm:text-sm">
-              {children}
-            </ListboxOptions>
-          </Transition>
-        </div>
-      </Listbox>
+    <Field className={twMerge("relative w-full p-3", className)}>
+      <div className="flex">
+        {label && (
+          <Label className="my-auto px-3 whitespace-nowrap text-sm font-medium leading-6 text-gray-900 dark:text-gray-200">
+            {label}
+          </Label>
+        )}
+        <Listbox value={value} onChange={onChange} disabled={disabled}>
+          <div className="relative">
+            <ListboxButton className="relative w-full cursor-default rounded-md bg-white dark:bg-gray-800 py-2 pl-3 pr-10 text-left text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6">
+              {selectedOption}
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </ListboxButton>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <ListboxOptions className="absolute z-10 mt-1 max-h-60 overflow-auto rounded-md bg-white dark:bg-gray-800 py-1 text-base shadow-lg ring-1 ring-black dark:ring-gray-700 ring-opacity-5 focus:outline-none sm:text-sm">
+                {children}
+              </ListboxOptions>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
     </Field>
   );
 }
